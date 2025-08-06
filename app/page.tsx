7aa566ac7c0116a06717b9d75b6aa5e177b0d4da
linkoraa <tmbekzod05@gmail.com>
2025-08-06 23:41:50 +0500

    fix i18n

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-'use client'
 import Nav from "@/components/core/nav";
 import DownloadAppSection from "@/components/pages/DownloadAppSection";
 import Features from "@/components/pages/Features";
 import Footer from "@/components/pages/Footer";
 import Hero from "@/components/pages/hero";
 import Pricing from "@/components/pages/Pricing";
-import { useTranslation } from "next-i18next";
 
 export default function Home() {
-  const { t } = useTranslation("common");
   return (
     <div className="relative w-full flex pt-[85px] flex-col min-h-screen bg-white overflow-hidden">
       <Nav />
@@ -28,11 +25,6 @@ export default function Home() {
       <div id="download" className="w-full px-6 py-6">
         <DownloadAppSection />
       </div>
-
-      {/* <div className="h-[200px]"></div> */}
-
-      <h1>{t("welcome")}</h1>
-      <p>{t("home")}</p>
 
       <Footer />
     </div>
